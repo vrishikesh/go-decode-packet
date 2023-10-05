b77158182bd84831faebead50b172b40d71a2e8e
vrishikesh <vrishikesh@outlook.com>
2023-10-05 20:27:44 +0530

    add comments

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 )
 
+// decodedPacket defines packet fields and types
 type decodedPacket struct {
 	Short1  int16
 	Twelve  string
@@ -16,12 +17,16 @@ type decodedPacket struct {
 	Long    int32
 }
 
+// decodePacket takes a byte slice packet as input and returns the decodedPacket
 func decodePacket(packet []byte) (decodedPacket, error) {
+	// as mentioned in the document the packet size will always be 44 bytes
 	if len(packet) != 44 {
 		return decodedPacket{}, fmt.Errorf("packet size should be 44 bytes")
 	}
 
+	// decode packet
 	p := decodedPacket{}
+	// usually for streaming in most cases BigEndian is used
 	p.Short1 = int16(binary.BigEndian.Uint16(packet[:2]))
 	p.Twelve = string(packet[2:14])
 	p.Single = packet[14]
@@ -34,6 +39,7 @@ func decodePacket(packet []byte) (decodedPacket, error) {
 }
 
 func main() {
+	// this could be from a stream
 	packet := []byte{
 		0x04, 0xD2,
 		0x6B, 0x65, 0x65, 0x70, 0x64, 0x65, 0x63, 0x6F, 0x64, 0x69, 0x6E, 0x67,
@@ -45,8 +51,10 @@ func main() {
 		// 0xFF, 0xFF, 0xFB, 0x2E, // -1234
 	}
 
+	// try to decode the packet
 	message, err := decodePacket(packet)
 	if err != nil {
+		// if could not decode the packet then log the error and fail
 		log.Fatalf("decoding failed: %s", err)
 	}
 
